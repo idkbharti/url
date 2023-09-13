eda00b2fc4e98bace43204a30012f42ace862abb
nubdevs <idkbharti@gmail.com>
2023-09-13 17:55:02 +0530

    final

--- controllers/qr.js
+++ controllers/qr.js
@@ -1,93 +1,90 @@
-const QRCode = require('qrcode-svg');
-const s3 = require('../aws-config');
+const QRCode = require("qrcode-svg");
+const s3 = require("../aws-config");
 
+async function handleQRGenerator(req, res) {
+  const { type, content } = req.body;
 
-async function handleQRGenerator(req,res){
-    const { type, content } = req.body;
+  let qrText = "";
+  switch (type) {
+    case "Text":
+      qrText = content;
+      break;
+    case "SMS":
+      qrText = `SMSTO:${content}`;
+      break;
+    case "Email":
+      qrText = `mailto:${content}`;
+      break;
+    case "Url":
+      qrText = content;
+      break;
+    case "Wifi":
+      const { ssid, password, isHidden } = content;
+      // console.log(content,ssid,password,isHidden)
+      const securityType = password ? "WPA" : "nopass";
+      qrText = `WIFI:T:${securityType};S:${ssid};P:${password};;${
+        isHidden ? "H:true" : ""
+      };`;
+      break;
+    default:
+      res.status(400).send("Invalid type");
+      return;
+  }
 
-    let qrText = '';
-    switch (type) {
-        case 'Text':
-            qrText = content;
-            break;
-        case 'SMS':
-            qrText = `SMSTO:${content}`;
-            break;
-        case 'Email':
-            qrText = `mailto:${content}`;
-            break;
-        case 'Url':
-            qrText = content;
-            break;
-        case 'Wifi':
-            const { ssid, password, isHidden } = content;
-            // console.log(content,ssid,password,isHidden)
-            const securityType = password ? 'WPA' : 'nopass';
-            qrText = `WIFI:T:${securityType};S:${ssid};P:${password};;${isHidden ? 'H:true' : ''};`;
-            break;
-        default:
-            res.status(400).send('Invalid type');
-            return;
-    }
-
-    const qr = new QRCode({
+  const qr = new QRCode({
     content: qrText,
     padding: 4,
     width: 200, // Increase the width
     height: 200, // Increase the height
-    color: '#000000',
-    background: '#ffffff'
-    });
+    color: "#000000",
+    background: "#ffffff",
+  });
 
-    // Send the QR code SVG as response
-    res.setHeader('Content-Type', 'image/svg+xml');
+  // Send the QR code SVG as response
+  res.setHeader("Content-Type", "image/svg+xml");
+  res.send(qr.svg());
+}
+
+async function handleUploadqrGenerator(req, res) {
+  try {
+    if (!req.file) {
+      return res.status(400).json({ message: "No file uploaded." });
+    }
+
+    const { originalname, buffer } = req.file;
+    // console.log(req.file)
+
+    // Upload the file to Amazon S3
+    const params = {
+      Bucket: "snapurl",
+      Key: originalname,
+      Body: buffer,
+    };
+
+    const url = await s3.upload(params).promise();
+    // console.log(url)
+
+    // Generate the QR code
+    const param = {
+      Bucket: "snapurl",
+      Key: originalname,
+    };
+
+    const signedUrl = s3.getSignedUrl("getObject", param);
+    //  console.log(signedUrl)
+    // Generate the QR code for the signed URL
+    const qr = new QRCode(signedUrl);
+
+    // Send the QR code as an SVG response
+    res.type("svg");
     res.send(qr.svg());
-
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: "Internal Server Error" });
+  }
 }
 
-async function handleUploadqrGenerator(req,res){
-    try {
-        if (!req.file) {
-          return res.status(400).json({ message: 'No file uploaded.' });
-        }
-    
-        const { originalname, buffer } = req.file;
-        // console.log(req.file)
-        
-        // Upload the file to Amazon S3
-        const params = {
-          Bucket: 'snapurl',
-          Key: originalname,
-          Body: buffer,
-        };
-    
-        const url = await s3.upload(params).promise();
-        // console.log(url)
-    
-        // Generate the QR code
-        const param = {
-          Bucket: 'snapurl',
-          Key: originalname,
-        };
-    
-        const signedUrl = s3.getSignedUrl('getObject', param);
-      //  console.log(signedUrl)
-        // Generate the QR code for the signed URL
-        const qr = new QRCode(signedUrl);
-    
-        // Send the QR code as an SVG response
-        res.type('svg');
-        res.send(qr.svg());
-      } catch (error) {
-        console.error(error);
-        res.status(500).json({ message: 'Internal Server Error' });
-      }
-  
-}
-
-
-
-module.exports={
-    handleQRGenerator,
-    handleUploadqrGenerator
-}
+module.exports = {
+  handleQRGenerator,
+  handleUploadqrGenerator,
+};
--- index.js
+++ index.js
@@ -1,30 +1,26 @@
 const express = require("express");
-const {connectToDb}=require("./connect");
-const urlRoute = require("./routes/urlRoutes")
-const URL = require("./models/urlSchema")
-const cors = require("cors")
-const bodyParser = require('body-parser')
-const QRCode = require('qrcode-svg');
-const qrRoute = require("./routes/qrRoutes")
-const s3 = require('./aws-config');
+const { connectToDb } = require("./connect");
+const urlRoute = require("./routes/urlRoutes");
+const URL = require("./models/urlSchema");
+const cors = require("cors");
+const bodyParser = require("body-parser");
+const QRCode = require("qrcode-svg");
+const qrRoute = require("./routes/qrRoutes");
+const s3 = require("./aws-config");
 
-require('dotenv').config()
-connectToDb(process.env.DB_URL).then(()=>console.log("sucessfully connect to database"));
-const app=express();
+require("dotenv").config();
+connectToDb(process.env.DB_URL).then(() =>
+  console.log("sucessfully connect to database")
+);
+const app = express();
 app.use(express.json());
-app.use(cors())
+app.use(cors());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
-const PORT=process.env.PORT || 8003;
+const PORT = process.env.PORT || 8003;
 
+app.use("/url", urlRoute);
+app.use("/qr", qrRoute);
 
-app.use('/url',urlRoute);
-app.use('/qr',qrRoute);
-
-
-
-app.listen(PORT,()=>console.log(`server running on port ${PORT}`));
-
-
-
+app.listen(PORT, () => console.log(`server running on port ${PORT}`));
